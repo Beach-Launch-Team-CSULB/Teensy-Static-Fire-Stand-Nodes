83dbeb6af178a07b66fb7c713c888c12d5407987
Mat-Arnold <80731450+Mat-Arnold@users.noreply.github.com>
2021-08-30 12:37:02 -0700

    Update StateList.h

--- StaticFire_TeensyCode_v1/src/StateList.h
+++ StaticFire_TeensyCode_v1/src/StateList.h
@@ -6,18 +6,20 @@
 enum class State
 {
     debug,      //this is a debug state
-    // These are the God States, they can be reached from any position
-    abort,      // lox vent open,   fuel vent open,   lox iso open,   fuel iso open,   main lox closed, main fuel closed, iso enable on,  fuel vent enable on,  lox vent enable off, MV enable off
-    test,       // only this state allows individual actuation
+
     passive,    // lox vent open,   fuel vent closed, lox iso closed, fuel iso closed, main lox closed, main fuel closed, iso enable off, fuel vent enable off, lox vent enable off, MV enable off 
+    test,       // only this state allows individual actuation
+    loxLoad,    // lox vent open,   fuel vent closed, lox iso closed, fuel iso closed, main lox closed, main fuel closed, iso enable off, fuel vent enable off, lox vent enable on,  MV enable off
+    pressArm,   // lox vent closed, fuel vent closed, lox iso closed, fuel iso closed, main lox closed, main fuel closed, iso enable on,  fuel vent enable off, lox vent enable on,  MV enable off
+    pressurize, // lox vent closed, fuel vent closed, lox iso open,   fuel iso open,   main lox closed, main fuel closed, iso enable on,  fuel vent enable off, lox vent enable on,  MV enable off
+    abort,      // lox vent open,   fuel vent open,   lox iso open,   fuel iso open,   main lox closed, main fuel closed, iso enable on,  fuel vent enable on,  lox vent enable off, MV enable off
+    fireArmed,  // lox vent closed, fuel vent closed, lox iso open,   fuel iso open,   main lox closed, main fuel closed, iso enable on,  fuel vent enable off, lox vent enable on,  MV enable on
+    fire,       // lox vent closed, fuel vent closed, lox iso open,   fuel iso open,   main lox open,   main fuel open,   iso enable on,  fuel vent enable off, lox vent enable on,  MV enable on
     vent,       // lox vent open,   fuel vent open,   lox iso closed, fuel iso closed, main lox closed, main fuel closed, iso enable off, fuel vent enable on,  lox vent enable on,  MV enable off
     
-    // These states can only be accessed in sequence, from passive
-    loxLoad,    // lox vent open,   fuel vent closed, lox iso closed, fuel iso closed, main lox closed, main fuel closed, iso enable off, fuel vent enable off, lox vent enable on, MV enable off
-    pressArm,   // lox vent closed, fuel vent closed, lox iso closed, fuel iso closed, main lox closed, main fuel closed, iso enable on,  fuel vent enable off, lox vent enable on, MV enable off
-    pressurize, // lox vent closed, fuel vent closed, lox iso open,   fuel iso open,   main lox closed, main fuel closed, iso enable on,  fuel vent enable off, lox vent enable on, MV enable off
-    fireArmed,  // lox vent closed, fuel vent closed, lox iso open,   fuel iso open,   main lox closed, main fuel closed, iso enable on,  fuel vent enable off, lox vent enable on, MV enable on
-    fire,       // lox vent closed, fuel vent closed, lox iso open,   fuel iso open,   main lox open,   main fuel open,   iso enable on,  fuel vent enable off, lox vent enable on, MV enable on
+    
+    
+    
 
 };
 
