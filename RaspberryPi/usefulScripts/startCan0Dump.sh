805aa0666950141a059ffb58404fdf1fa607a871
Jacob Waters <fierywaters13@gmail.com>
2021-10-10 22:26:22 -0700

    removed Pi Scripts from this repo

--- RaspberryPi/usefulScripts/startCan0Dump.sh
+++
@@ -1,5 +0,0 @@
-#These commands can be run in a larger script in the background
-#but doing so runs the risk of the script being run twice,
-#with the result of having multiple copies of the process running
-#concurrently, which could have undesired behavior.
-candump -tA can0 >> can0Log.txt
